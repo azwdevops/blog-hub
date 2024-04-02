ae5d3d5007b71dea20bd44449588f58457cf6aec
Zachary Waweru <azwdevops@gmail.com>
2024-04-02 12:11:42 +0300

    added header component and items related to it

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
+import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div>
+    <>
+      <Header />
       <Outlet />
-    </div>
+    </>
   );
 };
 
--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,12 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "@/layouts/MainLayout";
 import Home from "@/pages/Home";
+import Signup from "@/pages/Signup";
+import Signin from "@/pages/Signin";
+import Dashboard from "@/pages/Dashboard";
+import Projects from "@/pages/Projects";
+import About from "@/pages/About";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [{ path: "", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "/signin", element: <Signin /> },
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/about", element: <About /> },
+      { path: "/projects", element: <Projects /> },
+    ],
   },
 ]);
 
